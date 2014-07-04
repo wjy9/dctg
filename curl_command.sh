08b17a8d1370ef0371e8f17b157557121d255e3b
tester <tester@cnpt>
2014-07-04 10:30:02 +0000

    curl_command.sh now uses xxx.json as its parameter

--- curl_command.sh
+++ curl_command.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-curl http://localhost:8080/ -X POST -d @http_config.json --header "Content-Type:application/json"
+curl http://localhost:8080/ -X POST -d @$1 --header "Content-Type:application/json"
 
 curl http://localhost:8080/status
 
